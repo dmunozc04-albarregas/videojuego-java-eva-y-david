e30303274d1d831f18211c2e235a151077ea891c
eva <evareamarmu@gmail.com>
2025-04-02 14:06:50 +0200

    Añadido método cargarEscenario en vista

--- Controlador.java
+++ Controlador.java
@@ -49,7 +49,5 @@ public class Controlador {
         }*/
     }
 
-    /*public void cargarEscenario(Path rutaEscenario) {
-        
-    }*/
+    
 }
--- Vista.java
+++ Vista.java
@@ -1,8 +1,32 @@
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.List;
 public class Vista {
-	private Controlador controlador;
 	
-	/*public void mostrarEscenario(File escenario) {
-		System.out.println(escenario);
-		System.out.println("\nESCENARIO CARGADO");
-	}*/
+	public void cargarEscenario(Path rutaEscenario) {
+        List<String> archivoEscenario;
+        int obsaculos = 0;
+        int espacios = 0;
+        try {
+        	if(Files.notExists(rutaEscenario)) {
+        		System.out.println("El archivo no existe.")
+        	} else {
+        		archivoEscenario = Files.readAllLines(rutaEscenario);
+
+        		for(String linea : archivoEscenario) {
+        			for(char caracter : linea.toCharArray()) {
+        			if(caracter == ¬) {
+        				obsaculos++;
+        			} else if(caracter == |) {
+        				espacios++;
+        			}
+        		}
+        	}
+        	System.out.println("Total de caracteres: " + totalCaracteres);
+            System.out.println("Obstáculos ('¬'): " + obstaculos);
+            System.out.println("Espacios (' '): " + espacios);
+        } catch(IOException e) {
+			e.printStackTrace();
+        }
+    }
 }
